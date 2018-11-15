9a4aab32fbcdabce9d32322d120bfd07576be4ec
KostadinYankov <32684892+KostadinYankov@users.noreply.github.com>
2018-11-15 17:35:18 +0000

    15/11/2018 CHANGES

--- ACME/searchPage.py
+++ ACME/searchPage.py
@@ -6,38 +6,44 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.base import runTouchApp
 from kivy.graphics import Color, Rectangle
+from kivy.uix.scatter import Scatter
 import json
 
 import backend
 
 if __name__ == '__main__':
-
     root = FloatLayout()
+
+
+
+
+
+    # # Find a random movie                 HERE JAMES CREATED THE 6 OBJECTS
+    # def findRandomMovie(*l):
+    #     for x in range(6):
+    #         ads = Button(
+    #             text='here',
+    #             size_hint=(.16, 0.075),
+    #             pos_hint={'x': (x/10), 'y': 1 - topPadding}
+    #         )
+    #         root.add_widget(ads)
+
+
+
+
+
+
+
 
     # Find a random movie
     def findRandomMovie(*l):
         db = "omdbapi"
         a = backend.APIReqRandom(db)
-        #print(json.dumps(a, sort_keys=True, indent=4, separators=(',', ': ')))
+        # print(json.dumps(a, sort_keys=True, indent=4, separators=(',', ': ')))
         label.text = json.dumps(a, indent=4, separators=(',', ': '))
-
-
-    # Find a movie
-    def findMovie(*l):
-        db = "omdbapi"
-        searchtype = "s"
-        searchval = inputBox.text
-        a = backend.APIReq(db, searchval, searchtype)
-        print(len(a))
-        label.text = json.dumps(a, indent=4, separators=(',', ': '))
-        #print(json.dumps(a, sort_keys=True, indent=4, separators=(',', ': ')))
-
-    topPadding = .1
-
-
-
 
     # Define a button object and attach a script
+    topPadding = .1  # just test here with the padding
     rndButton = Button(
         text='Find random \nmovie',
         size_hint=(.16, .075),
@@ -48,12 +54,19 @@ if __name__ == '__main__':
     )
     root.add_widget(rndButton)
 
+    # Search label
+    def findMovie(*l):
+        db = "omdbapi"
+        searchtype = "s"# s stands for search
+        searchval = inputBox.text
+        a = backend.APIReq(db, searchval, searchtype)
+        print(len(a))
+        label.text = json.dumps(a, indent=4, separators=(',', ': '))
+        #print(json.dumps(a, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
 
-
-
-    # Define Input box in top middle of screen
+    # Define Input box in top middle of screen , this is for the search label
     inputBox = TextInput(
         size_hint=(0.5, 0.075),
         pos_hint={'x': .2, 'y': 1-topPadding},
@@ -62,10 +75,7 @@ if __name__ == '__main__':
     )
     root.add_widget(inputBox)
 
-
-
-
-    # Define a button object and attach a script
+    # Define a button object and attach a script, this searches through OMDBapi
     searchButton = Button(
         text='SEARCH',
         size_hint=(.1, .075),
@@ -79,34 +89,32 @@ if __name__ == '__main__':
 
 
 
+
+
+
     # Define a button object and attach a script
     listButton = Button(
         text='MY LIST',
         size_hint=(.16, 0.075),
         pos_hint={'x': .82, 'y': 1-topPadding}
     )
-    listButton.bind(
-        on_release=findRandomMovie
-    )
+    # listButton.bind(
+    #     on_release=findRandomMovie   # add function to open mylist page
+    # )
     root.add_widget(listButton)
 
-
-
-
-
     # Define Textbox at top of Window
-    labelText = 'Search for Output'
+    labelText = 'Output here' # this displays the searched information
     label = Label(
         text=labelText,
         size_hint_y=None,
         height=100,
         pos_hint={'top': .5},
         color=(0, 1, 0, 1),
-        halign = 'center',
-        valign = 'center'
+        halign='center',
+        valign='center'
     )
     root.add_widget(label)
-
     runTouchApp(root)
 
 
