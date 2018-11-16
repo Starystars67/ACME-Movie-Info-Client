33b27f7a6cfc93eb17b1bea81172b1fbdd53eb0c
Starystars67 <starystars67@gmail.com>
2018-11-16 09:21:16 +0000

    updatede kivy install and added details page

--- ACME/Install_Kivy.bat
+++ ACME/Install_Kivy.bat
@@ -1,27 +1,29 @@
 @echo off
 
+set /P loc=please enter your python location: 
+
 echo INFO: Installing latest pip and wheel.
-C:\Python27amd64\python.exe -m pip install --upgrade pip wheel setuptools
+%loc%\python.exe -m pip install --upgrade pip wheel setuptools
 goto Dependencies
 
 :Dependencies
 echo INFO: Installing docutils pygments.
-C:\Python27amd64\python.exe -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+%loc%\python.exe -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
 echo INFO: Installing kivy.deps.gstreamer
-C:\Python27amd64\python.exe -m pip install kivy.deps.gstreamer
+%loc%\python.exe -m pip install kivy.deps.gstreamer
 goto Angle
 
 :Angle
 echo INFO: Installing angle backend.
-C:\Python27amd64\python.exe -m pip install kivy.deps.angle
+%loc%\python.exe -m pip install kivy.deps.angle
 goto Kivy
 
 :Kivy
 echo.
 echo INFO: Installing Kivy.
-C:\Python27amd64\python.exe -m pip install kivy
-goto Examples
+%loc%\python.exe -m pip install kivy
+REM goto Examples
 
 :Examples
 echo INFO: Installing Kivy examples.
-C:\Python27amd64\python.exe -m pip install kivy_examples
+%loc%\python.exe -m pip install kivy_examples
